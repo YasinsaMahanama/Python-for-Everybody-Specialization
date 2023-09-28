b0155914c54a4d7845be5f1fd32e8903b80c294b
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-09-28 12:22:52 +0530

    Update mnemonic.py

--- Programming for Everybody (Getting Started with Python)/week 4/mnemonic.py
+++ Programming for Everybody (Getting Started with Python)/week 4/mnemonic.py
@@ -1,3 +1,14 @@
+x1q3z9ocd = 35.0
+x1q3z9afd = 12.50
+x1q3p9afd = x1q3z9ocd * x1q3z9afd
+print(x1q3p9afd)
+
+a = 35.0
+b = 12.50
+c = a * b
+print(c)
+
+
 hours = 35.0
 rate = 12.50
 pay = hours * rate
