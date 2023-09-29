75cd2d6114884ba686d1e92c52d0df605e1c0c9c
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-09-29 22:03:49 +0530

    Create Multi-Way.py

---
+++ Programming for Everybody (Getting Started with Python)/week 5/Multi-Way.py
@@ -0,0 +1,11 @@
+A = input('Enter the value of x : ')
+print('x = ',A)
+x = int(A)
+if x < 2 :
+    print('Small')
+else :
+    if x <10 :
+        print('Medium')
+    else :
+        print('Large')
+print('All Done')
