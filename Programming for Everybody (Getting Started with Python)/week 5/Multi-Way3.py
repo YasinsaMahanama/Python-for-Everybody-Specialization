2e79419d2bde5c7ef9cd8dcfbd230c0fe8bf18a4
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-09-29 22:18:07 +0530

    Create Multi-Way3.py

---
+++ Programming for Everybody (Getting Started with Python)/week 5/Multi-Way3.py
@@ -0,0 +1,16 @@
+A = input('Enter the value of x : ')
+print('x = ',A)
+x = int(A)
+if x < 2 :
+    print('Small')
+elif x < 10 :
+    print('Medium')
+elif x < 20 :
+    print('Big')
+elif x < 40 :
+    print('Large')
+elif x < 100 :
+    print('Huge')
+else :
+    print('Ginormous')
+print('All Done')
