ad6b8956f24563e3aed50782113ed156eb66ed3e
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-09-29 21:06:22 +0530

    Create One-way decisions.py

---
+++ Programming for Everybody (Getting Started with Python)/week 5/One-way decisions.py
@@ -0,0 +1,14 @@
+x = 5
+print('Before 5')
+if x == 5 :
+    print('Is 5')
+    print('Is still 5')
+    print('Third 5')
+print('Aferwars 5')
+print('Before 6')
+if x == 6 :
+    print('Is 6')
+    print('Is still 6')
+    print('Third 6')
+print('Aferwars 6')
+    
