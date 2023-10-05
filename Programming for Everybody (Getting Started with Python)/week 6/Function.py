ad15ad74cb785f42d6eaf1179ac2a3068864da69
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-05 16:40:02 +0530

    Create Function.py

---
+++ Programming for Everybody (Getting Started with Python)/week 6/Function.py
@@ -0,0 +1,10 @@
+x = 5
+print('Hello')
+
+def print_lyrrics() :
+    print("I'm a lumberjack, and I'm okay")
+    print('I sleep all night and I work all day')
+print('Yo')
+print_lyrrics()
+x = x + 2
+print(x)
